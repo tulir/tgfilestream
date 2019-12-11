a87c2154dd6ba53b3ed1f9ca4632409a7aaaf6eb
Tulir Asokan <tulir@maunium.net>
2019-12-11 18:32:57 +0200

    Make basic functionality work

--- tgfilestream.py
+++ tgfilestream.py
@@ -1,8 +1,11 @@
+from typing import Tuple, Union, cast
 import asyncio
 import sys
 import os
 
 from telethon import TelegramClient, events
+from telethon.tl.custom import Message
+from telethon.tl.types import TypeInputPeer, InputPeerChannel, InputPeerChat, InputPeerUser
 from aiohttp import web
 
 try:
@@ -12,27 +15,116 @@ except (KeyError, ValueError):
     print("Please set the TG_API_ID and TG_API_HASH environment variables correctly")
     sys.exit(1)
 
+try:
+    port = int(os.environ.get("PORT", "8080"))
+except ValueError:
+    port = -1
+if not 1 <= port <= 65535:
+    print("Please make sure the PORT environment variable is an integer between 1 and 65535")
+    sys.exit(1)
+
+host = os.environ.get("HOST", "localhost")
+public_url = os.environ.get("PUBLIC_URL", f"http://{host}:{port}")
 session_name = os.environ.get("TG_SESSION_NAME", "tgfilestream")
-loop = asyncio.get_event_loop()
-client = TelegramClient(session_name, api_id, api_hash, loop=loop)
+client = TelegramClient(session_name, api_id, api_hash)
 routes = web.RouteTableDef()
+
+pack_bits = 32
+pack_bit_mask = (1 << pack_bits) - 1
+
+group_bit = 0b01
+channel_bit = 0b10
+chat_id_offset = 2
+msg_id_offset = pack_bits + chat_id_offset
+
+
+def pack_id(evt: events.NewMessage.Event) -> int:
+    file_id = 0
+    if evt.is_group:
+        file_id |= group_bit
+    if evt.is_channel:
+        file_id |= channel_bit
+    file_id |= evt.chat_id << chat_id_offset
+    file_id |= evt.id << msg_id_offset
+    return file_id
+
+
+def unpack_id(file_id: int) -> Tuple[TypeInputPeer, int]:
+    is_group = file_id & group_bit
+    is_channel = file_id & channel_bit
+    chat_id = file_id >> chat_id_offset & pack_bit_mask
+    msg_id = file_id >> msg_id_offset & pack_bit_mask
+    if is_channel:
+        peer = InputPeerChannel(channel_id=chat_id, access_hash=0)
+    elif is_group:
+        peer = InputPeerChat(chat_id=chat_id)
+    else:
+        peer = InputPeerUser(user_id=chat_id, access_hash=0)
+    return peer, msg_id
+
+
+def get_file_name(message: Union[Message, events.NewMessage.Event]) -> str:
+    if message.file.name:
+        return message.file.name
+    ext = message.file.ext or ""
+    return f"{message.date.strftime('%Y-%m-%d_%H:%M:%S')}{ext}"
 
 
 @client.on(events.NewMessage)
 async def handle_message(evt: events.NewMessage.Event) -> None:
-    pass
+    if not evt.is_private or not evt.file:
+        return
+    url = f"{public_url}/{pack_id(evt)}/{get_file_name(evt)}"
+    await evt.reply(f"Link to download file: [{url}]({url})")
 
 
 @routes.get(r"/{id:\d+}/{name}")
 async def handle_request(req: web.Request) -> web.Response:
-    msg_id = int(req.match_info["id"])
     file_name = req.match_info["name"]
-    return web.Response(status=501)
+    file_id = int(req.match_info["id"])
+    peer, msg_id = unpack_id(file_id)
+    if not peer or not msg_id:
+        return web.Response(status=404, text="404: Not Found")
+    message = cast(Message, await client.get_messages(entity=peer, ids=msg_id))
+    if not message or not message.file or get_file_name(message) != file_name:
+        return web.Response(status=404, text="404: Not Found")
+    return web.Response(status=200, body=client.iter_download(message.media),
+                        headers={
+                            "Content-Type": message.file.mime_type,
+                            "Content-Length": str(message.file.size),
+                            "Content-Disposition": f'attachment; filename="{file_name}"'
+                        })
 
 
 server = web.Application()
 server.add_routes(routes)
+runner = web.AppRunner(server)
 
-client.start()
-web.run_app(server)
-client.disconnect()
+loop = asyncio.get_event_loop()
+
+
+async def start() -> None:
+    await client.start()
+
+    await runner.setup()
+    site = web.TCPSite(runner, host, port)
+    await site.start()
+
+
+async def stop() -> None:
+    await runner.cleanup()
+    await client.disconnect()
+
+
+loop.run_until_complete(start())
+print("========== Initialization complete ==========")
+print(f"Listening at http://{host}:{port}")
+print(f"Public URL prefix is {public_url}")
+print("(Press CTRL+C to quit)")
+try:
+    loop.run_forever()
+except KeyboardInterrupt:
+    loop.run_until_complete(stop())
+except Exception:
+    print("Fatal error in event loop")
+    raise

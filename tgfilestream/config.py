894cf7474373eb31d347aca6a76d36145d826290
Shrimadhav U K <shrimadhavuk@gmail.com>
2019-12-29 19:41:22 +0530

    The messages for group chats and non-media files should be different

--- tgfilestream/config.py
+++ tgfilestream/config.py
@@ -59,3 +59,4 @@ except ValueError:
 
 
 start_message = os.environ.get("TG_START_MESG", "Send an image or file to get a link to download it")
+group_chat_message = os.environ.get("TG_G_C_MESG", "Sorry. But, I only work in private.")
--- tgfilestream/telegram.py
+++ tgfilestream/telegram.py
@@ -19,7 +19,14 @@ from telethon import TelegramClient, events
 from telethon.sessions import StringSession
 
 from .paralleltransfer import ParallelTransferrer
-from .config import session_name, api_id, api_hash, public_url, start_message
+from .config import (
+    session_name,
+    api_id,
+    api_hash,
+    public_url,
+    start_message,
+    group_chat_message
+)
 from .util import pack_id, get_file_name
 
 log = logging.getLogger(__name__)
@@ -30,7 +37,10 @@ transfer = ParallelTransferrer(client)
 
 @client.on(events.NewMessage)
 async def handle_message(evt: events.NewMessage.Event) -> None:
-    if not evt.is_private or not evt.file:
+    if not evt.is_private:
+        await evt.reply(group_chat_message)
+        return
+    if not evt.file:
         await evt.reply(start_message)
         return
     url = public_url / str(pack_id(evt)) / get_file_name(evt)
